51bf4ada0ba44e7085303aa2a11cb8836a362bb9
Sayed-Noman <Sayed.siddik@northsouth.edu>
2020-11-17 12:05:44 +0600

    Homework 1 and 2 done

--- Homework Submission/Homework 1/2.asm
+++ Homework Submission/Homework 1/2.asm
@@ -2,15 +2,24 @@
 ; You may customize this and other start-up templates; 
 ; The location of this template is c:\emu8086\inc\0_com_template.txt
 ; Question: Allocate  even integers(starting from 2) in AX register and store theri sum ix CX register
-org 100h                                          
+org 100h
 
-    MOV AX, 02  ; AX <-- 02
-    ADD AX, 04  ; AX= 02+04
-    ADD AX, 06  ; AX= 06+06
-    ADD AX, 08  ; AX= 0C+08
-    ADD AX, 10  ; AX= 14+10 = 1E
+    MOV CX, 0   ; CX <-- 0
     
-    MOV CX, AX  ; CX <-- 1E
+    MOV AX, 02  ; AX <-- 02
+    ADD CX, AX  ; CX= 00+02
+    
+    MOV AX, 04  ; AX <-- 04
+    ADD CX, AX  ; CX= 02+04 
+    
+    MOV AX, 06  ; AX <-- 06
+    ADD CX, AX  ; CX= 06+06 
+    
+    MOV AX, 08  ; AX <-- 08
+    ADD CX, AX  ; CX= 0C+08 
+    
+    MOV AX, 10  ; AX <-- 10
+    ADD CX, AX  ; CX= 14+10=1E
 
 ret
 
